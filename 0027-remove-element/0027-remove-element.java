4d80f53ddbd5725cbade99900be82b6ef0fe0270
Nilam Bhapkar <x20145331@student.ncirl.ie>
2026-04-15 18:29:12 +0100

    Time: 4 ms (1.47%), Space: 43.7 MB (28.94%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int i=0;
+        for (int j=0;j<n;j++)
+        {
+            if(nums[j]!=val)
+            {
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
